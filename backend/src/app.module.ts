2e9bce9d100f26ba90154b3cbbb08d0f132b1b7e
Le Van Quyen <levanquyencontacts@gmail.com>
2026-06-04 19:12:58 +0700

    fix: update TypeOrmModule import and database configuration for better flexibility

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,27 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { FileModule } from './modules/files/file.module';
 import { UsersModule } from './modules/users/users.module';
-
 @Module({
-  imports: [FileModule, UsersModule, CategoriesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    username: 'postgres',
-    password: '123456',
-    database: 'chenow',
-    autoLoadEntities: true,
-    synchronize: true,
-    entities: [],
-  }), UsersModule, AuthModule],
+  imports: [
+    FileModule,
+    UsersModule,
+    CategoriesModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
