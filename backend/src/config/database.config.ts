6389c574a242cdff4ddb60e040ecd94f3589dddd
Le Van Quyen <levanquyencontacts@gmail.com>
2026-07-05 20:38:30 +0700

    remove log database

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -5,15 +5,6 @@ import { resolve } from 'path';
 dotenv.config({ path: resolve(process.cwd(), '.env'), override: true });
 
 export const getDatabaseConfig = (): TypeOrmModuleOptions => {
-  console.log('[Database]', {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    database: process.env.DB_DATABASE,
-    passwordLength: process.env.DB_PASSWORD?.length,
-    passwordMatchesExpected: process.env.DB_PASSWORD === 'chenow',
-  });
-
   return {
     type: 'postgres',
     host: process.env.DB_HOST,
